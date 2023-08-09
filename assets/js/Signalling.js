66a87b270882a4dc5da88f9b7c69efd85736ecc2
Cleaza <107327396+CleazaMain@users.noreply.github.com>
2023-08-09 16:13:03 +0300

    Update Signalling.js

--- assets/js/Signalling.js
+++ assets/js/Signalling.js
@@ -1,28 +1,30 @@
-'use strict'
+'use strict';
 Pusher.logToConsole = false;
-let channelName, pusher, base = "https://cleazamain.github.io/CleazaX/";
+let channelName, pusher, base = "https://cleazax.netlify.app/";
+
 fetch(base + "/connect" + (localStorage.id ? `?id=${localStorage.id}` : ""), {
     "method": "GET",
     "headers": {
         "Content-Type": "application/x-www-form-urlencoded"
     }
 })
-    .then(function (response) {
-        console.log(response.status);
-        return response.json();
-    }).then(function (data) {
-        if (!localStorage.id || localStorage.id != data.id){
-            localStorage.id = data.id;
-            updateTooltip();}
-        channelName = data.channel;
-        pusher = new Pusher('892a123070fc89c43124', {
-            cluster: 'ap2'
-        });
-        subscribe();
-    })
-    .catch(function (error) {
-        console.log(error.message);
+.then(function (response) {
+    console.log(response.status);
+    return response.json();
+}).then(function (data) {
+    if (!localStorage.id || localStorage.id != data.id){
+        localStorage.id = data.id;
+        updateTooltip();
+    }
+    channelName = data.channel;
+    pusher = new Pusher('892a123070fc89c43124', {
+        cluster: 'ap2'
     });
+    subscribe();
+})
+.catch(function (error) {
+    console.log(error.message);
+});
 
 function subscribe() {
     window.channel = pusher.subscribe(channelName);
@@ -30,12 +32,9 @@ function subscribe() {
     window.channel.bind('pusher:subscription_error', () => { setTimeout(subscribe, 1000); });
     window.channel.bind('message', (mes) => signallingOnMessage(mes));
 }
-async function signal(mes) {
-    return await fetch(base + "/connect", {
-        "method": "POST",
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": JSON.stringify({ "id": mes.id, "message": mes })
-    });
+
+// signallingOnMessage ve diğer fonksiyonlarınızı burada tanımlayabilirsiniz
+
+function updateTooltip() {
+    // Tooltip güncelleme işlemleri burada olabilir
 }
